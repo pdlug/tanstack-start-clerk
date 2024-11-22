c51e567e462521febe573691978bd6d709c032ef
Paul Dlug <paul.dlug@gmail.com>
2024-11-22 10:47:03 -0500

    Initial commit

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "@tanstack/start/config";
+
+import { cloudflare } from "unenv";
+
+export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+    unenv: cloudflare,
+  },
+});
